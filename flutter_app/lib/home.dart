6d676a6ba9297bde86afaec8ce397cdf0fbeef08
longtkhd <longtkhd2709@gmail.com>
2020-04-10 22:07:46 +0700

    set colorBg

---
+++ flutter_app/lib/home.dart
@@ -0,0 +1,16 @@
+
+class Home extends StatefulWidget {
+ 
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
